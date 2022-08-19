9b9be63cfd46dd83984b339abf3322f339af840a
Nicole Fernanda Miranda de Arruda <nicole.arruda@in.cassems.com.br>
2022-08-19 14:00:21 -0400

    projeto utilizando nestj aplicando o prisma

---
+++ src/book/book.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { BookDTO } from './book.dto';
+import { BookService } from './book.service';
+
+@Controller('book')
+export class BookController {
+  constructor(private readonly bookService: BookService) { }
+  
+  @Post()
+  async create(@Body() data: BookDTO) {
+    return this.bookService.create(data);
+  }
+}
